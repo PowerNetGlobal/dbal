96d19fa4593415219924425d6ccab3470ed52c59
Rob Hilgefort <rjhilgefort@gmail.com>
2013-07-22 12:38:15 -0400

    tweaking NLS_DATE_FORMAT

--- lib/Doctrine/DBAL/Event/Listeners/OracleSessionInit.php
+++ lib/Doctrine/DBAL/Event/Listeners/OracleSessionInit.php
@@ -44,7 +44,7 @@ class OracleSessionInit implements EventSubscriber
      */
     protected $_defaultSessionVars = array(
         'NLS_TIME_FORMAT' => "HH24:MI:SS",
-        'NLS_DATE_FORMAT' => "YYYY-MM-DD HH24:MI:SS",
+        'NLS_DATE_FORMAT' => "YYYY-MM-DD", //RJH
         'NLS_TIMESTAMP_FORMAT' => "YYYY-MM-DD HH24:MI:SS",
         'NLS_TIMESTAMP_TZ_FORMAT' => "YYYY-MM-DD HH24:MI:SS TZH:TZM",
         'NLS_NUMERIC_CHARACTERS' => ".,",
